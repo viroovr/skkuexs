9bdea6b45551de40643d923cf026d7b1aa559885
mistypath <twinphill@g.skku.edu>
2023-05-21 05:05:01 +0900

    Apply css, Add rank function

--- forums/views.py
+++ forums/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
 
+import requests
+import json
+
 def main(request, school_name):
-    return render(request, 'forums/main.html', {'school_name': school_name})
+
+    # data = {
+    #     'key': value
+    # }
+    # url = 'https://~~'
+    # response = requests.get(url,params=data)
+    # context = json.loads(response.text)
+
+    context = {
+        'school_name': school_name,
+        'rank': 4
+    }
+    return render(request, 'forums/main.html', context)
+
+    # return render(request, 'forums/main.html', {'school_name': school_name})
