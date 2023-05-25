fc4c884461f74575a1465b12dfa23f13ce0d8141
viroovr <tjgusdnjs789@gmail.com>
2023-05-25 16:54:12 +0900

    로그인 완료 시 메인페이지 redirect 안가기

--- skkuexs/views/base_views.py
+++ skkuexs/views/base_views.py
@@ -1,7 +1,10 @@
 from django.core.paginator import Paginator
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render, get_object_or_404, redirect
 
 def index(request):
-    return render(request, "skkuexs/main.html")
+    # 로그인 되면 redirect할 곳
+    # if request.user.is_authenticated:
+    #     return redirect('forums/abc')
+    # else:
+        return render(request, "skkuexs/main.html")
