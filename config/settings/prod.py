b5bb75ae472d75c34261eb79b9036671e73b70c0
viroovr <tjgusdnjs789@gmail.com>
2023-05-30 00:23:36 +0900

    s add

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,4 +4,4 @@ ALLOWED_HOSTS = ['43.200.137.150', 'skkuexs.com']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
-BASE_URL = 'https://skkuex.com/'
+BASE_URL = 'https://skkuexs.com/'
