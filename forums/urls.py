95c76d2ae9e30c55d99c25c70991f73dcdcbf4cc
fun-soul <soha9801@g.skku.edu>
2023-06-05 16:25:59 +0900

    <feat> 학교생활 기타페이지 url 연결

--- forums/urls.py
+++ forums/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('<str:school_name>/submain_uni_life', views.submain_uni_life, name='submain_uni_life'),
     path('<str:school_name>/courses', views.courses, name='courses'),
     path('<str:school_name>/uni_review', views.uni_review, name='uni_review'),
-    # 주석 지우고 여기에 학교생활/기타페이지 추가해주세요
-    
+    path('<str:school_name>/etc_uni', views.etc_uni, name='etc_uni'),
     path('<str:school_name>/community', views.community, name='community'),
+    
 ]
